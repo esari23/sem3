05ea42ed24bc9e89b41867540f7b81a80cf87ae1
esari23 <77049630+esari23@users.noreply.github.com>
2021-09-30 10:21:27 +0530

    Create visit.py

---
+++ visit.py
@@ -0,0 +1,7 @@
+day = str(input())
+if day == 'sunday'or 'monday' or 'tuesday':
+  print("Go to mrs.inder's place")
+elif day == 'saturday':
+  print("Go to an outing")
+else:
+  print("Go to mr.inder's place")
